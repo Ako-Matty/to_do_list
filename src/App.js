af863783f5760f94892e850a851eec5a4160269f
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-02-19 14:50:21 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const  App = () => {
+  const [inputtext, setinputtext] = useState("");
+  const [items, setItems] = useState([]);
+
+  const[toggle, setToggle] = useState(false);
+
+ 
+  const AddItem = () => {
+console.log(items)
+    
+    let listCopy = [...items];
+    
+
+
+    listCopy.push(inputtext)
+    setItems(listCopy);
+
+  }
+  
+// Delete items
+
+const DeleteItem = (selectedIndex) => {
+
+let listCopy = [...items];
+
+listCopy.splice(selectedIndex, 1);
+
+setItems( listCopy)
+
+}
+
+const Crossitem = inputtext => {
+  console.log(Crossitem)
+
+  setToggle(!toggle)
+
+  let listCopy = [...items];
+
+  console.log(inputtext.target.parentElement)
+
+  if (inputtext.target.parentElement.style.textDecoration === "line-through") {
+    inputtext.target.parentElement.style.setProperty(`text-decoration`, "none");
+
+
+    
+  } else {
+    inputtext.target.parentElement.style.setProperty('text-decoration', 'line-through');
+  }
+
+}
+
+
+
+
+
   return (
+
+// Current list for editing
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <h1 className='Title'>My list to do</h1>
+      </div>
+
+   <div className='Addinglist'>   
+   
+
+   <input id='box' type="text" placeholder='Add to list' value={inputtext} onChange={ (event)=> setinputtext(event.target.value)} ></input>
+
+   <button id='Plus' onClick={AddItem}>Add list</button>
+   
+   
+
+<div className='texts'>{items.map( (item, index) => {
+return <div id='colourbox'> <h4 key={index} id="checkedlist"> {item} 
+ <button id='ticked' onClick = {Crossitem}>Complete</button> <button id='Minus' onClick = {() => DeleteItem(index)}>X</button> </h4> </div>
+
+})}</div>
+
+   </div>
     </div>
-  );
+
+
+  )
+
+
+
+
+
+
+
 }
 
 export default App;
